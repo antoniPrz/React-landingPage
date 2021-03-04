c300435e9d078a0def7ecb59ffea4b926c21f73c
antoni perez <antoni.protoni@gmail.com>
2021-03-03 21:50:34 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Cards from "./Cards";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Section1 from "./Section1";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+    <Navbar/>
+    <Section1/>
+    <Cards/>
+    <Footer/>
     </div>
+
+
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React  from "react";
+
+
+
+
+const Footer = () => {
+    return (
+
+        <div class="footer-copyright text-center py-3 p-3 mb-2 bg-dark text-white">.bg-dark>© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> myWEBSITE</a>
+  </div>
+        );
+    };
+  
+  export default Footer;
